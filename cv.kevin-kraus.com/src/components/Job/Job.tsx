fb293d97010123238a5cd1fd2d53f8bceac88e10
Kevin Kraus <kevin.kraus1@outlook.com>
2020-11-25 01:59:24 +0100

    feature: added key for "today"
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

--- cv.kevin-kraus.com/src/components/Job/Job.tsx
+++ cv.kevin-kraus.com/src/components/Job/Job.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {useTranslation} from 'react-i18next'
 import {Job} from "../../models/Job";
+import {TFunction} from "i18next";
 
 export const JobComponent = () => {
-    let translatedJobs: Array<Job> = useTranslation('translation').t('Jobs');
+    let t : TFunction;
+    t = useTranslation('translation').t;
+    let translatedJobs: Array<Job> = t('Jobs');
 
 
     return (
@@ -13,7 +16,7 @@ export const JobComponent = () => {
                     <h1>{job.COMPANY_NAME}</h1>
                     <h2>{job.COMPANY_ZIP} {job.COMPANY_CITY}</h2>
                     <h3>{job.JOB_TITLE}</h3>
-                    <h4>{job.FROM} - {job.CURRENT ? "now" : job.UNTIL}</h4>
+                    <h4>{job.FROM} - {job.CURRENT ? t("UNTIL_NOW") : job.UNTIL}</h4>
                     <h5>{job.DESCRIPTION}</h5>
                 </div>
             ))}
